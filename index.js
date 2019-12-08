c8fe0560409781cdeec5ec90345b5a34da11bfa0
adamissam <issamadam15@gmail.com>
2019-12-08 19:57:02 +0100

    connect to database, serve items

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express= require('express')
 const app = express()
+//const models = require('./data-player/models')
 const port = process.env.PORT || 8007
-app.get('/',(req,res)=>{
-    res.send({message:'Hello Word!!'})
-})
+const { Item } = require('./data-player/models');
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`)
-})
+
+
+app.get('/api/items', async (req, res) => {
+  res.send({ items: await Item.findAll() });
+});
+
+const PORT = process.env.PORT || 8007;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
